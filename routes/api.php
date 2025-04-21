cc730c748a1227e21286b3fa98a44ee819145376
Irfan <irfangunaw31@gmail.com>
2025-04-21 16:53:09 +0700

    conf_docker

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/categories', function () {
+    return response()->json([
+        'categories' => [
+            [
+                'id' => 1,
+                'name' => 'Electronics',
+                'is_featured' => true,
+            ],
+            [
+                'id' => 2,
+                'name' => 'Furniture',
+                'is_featured' => false,
+            ],
+            [
+                'id' => 3,
+                'name' => 'Clothing',
+                'is_featured' => true,
+            ],
+        ],
+    ]);
+});
